49b3ecfc6dda2c485b02fabb8b5e01f4e31799a6
MasaudNadaf <masaudnadaf930@gmail.com>
2025-04-23 12:48:03 +0530

    Add files via upload

---
+++ Testmethods/Seleniumwitnapache/src/parellelexcu/NewTest.java
@@ -0,0 +1,18 @@
+package parellelexcu;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NewTest {@Test		
+  
+}
+	
+
+	
+	
+	
