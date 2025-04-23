088c2440136447c096479e5ec7bc884064d7d752
MasaudNadaf <masaudnadaf930@gmail.com>
2025-04-23 12:47:39 +0530

    Add files via upload

---
+++ Updeted/src/main/java/NewMaven/Updeted/App.java
@@ -0,0 +1,13 @@
+package NewMaven.Updeted;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
